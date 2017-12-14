f37e852a516f797817a22c052c68d38e1cfaed57
shindyu <shindyu@shindyu-no-MacBook-Pro.local>
2017-12-14 23:44:10 +0900

    fix typo

--- ABTesting/ABTestingExtension.swift
+++ ABTesting/ABTestingExtension.swift
@@ -14,25 +14,25 @@ fileprivate struct DataStoreForTesting {
 }
 
 extension ABTesting {
-    fileprivate static func methodSwizzeling() {
-        if !DataStoreForTesting.wasMethodSwizzling {
-            DataStoreForTesting.wasMethodSwizzling = true
-            let originalMethod = class_getClassMethod(ABTesting.self, #selector(ABTesting.getBucketId(from:)))
-            let swizzledMethod = class_getClassMethod(ABTesting.self, #selector(ABTesting.getBucketIdForTesting(from:)))
-            method_exchangeImplementations(originalMethod!, swizzledMethod!)
-        }
-    }
-
-    class func setupForTesting() {
-        DataStoreForTesting.buckets = [:]
-        methodSwizzeling()
-    }
-
     @objc dynamic class func getBucketIdForTesting(from str: String) -> String {
         return DataStoreForTesting.buckets[str] ?? ""
     }
 
     class func setBucketIdForTesting(with bucketId: String, to e: Experiment) {
         DataStoreForTesting.buckets.updateValue(bucketId, forKey: e.rawValue)
+    }
+
+    class func setupForTesting() {
+        DataStoreForTesting.buckets = [:]
+        methodSwizzling()
+    }
+
+    fileprivate class func methodSwizzling() {
+        if !DataStoreForTesting.wasMethodSwizzling {
+            DataStoreForTesting.wasMethodSwizzling = true
+            let originalMethod = class_getClassMethod(ABTesting.self, #selector(ABTesting.getBucketId(from:)))
+            let swizzledMethod = class_getClassMethod(ABTesting.self, #selector(ABTesting.getBucketIdForTesting(from:)))
+            method_exchangeImplementations(originalMethod!, swizzledMethod!)
+        }
     }
 }
